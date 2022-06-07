ecbd3524fab9816c7e83b1f198c4ad0bdc93ede9
eliasfernandescout <eliasfernandescout@gmail.com>
2022-06-06 21:34:20 -0300

    add stacks js

---
+++ pilhas/stacks.js
@@ -0,0 +1,16 @@
+//LIFO - Last in first out - O PRIMEIRO QUE ENTRA EH O PRIMEIRO QUE SAI;
+
+const vetor = []
+vetor.push(1)
+vetor.push(2)
+
+console.log(vetor)
+console.log(vetor.pop())
+console.log(vetor)
+
+const Stack = () =>{
+    const data = []
+    let top = -1
+
+    return 
+}
